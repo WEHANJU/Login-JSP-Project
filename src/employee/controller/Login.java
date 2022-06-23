8561b4f5355b2d230adf0caf754fb3a27fd8381c
Ye Yeong Kwak <aesop0817@gmail.com>
2022-06-23 18:23:03 +0900

    modify
    modify Loginservlet

--- src/employee/controller/Login.java
+++ src/employee/controller/Login.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +34,16 @@ public class Login extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		String id = request.getParameter("employeeId");
 		String pw = request.getParameter("employeePasswd");
+		Cookie cookie=null;
+		//로그인 성공하면 Cookie 생성되도록
+		//아이디가 비어있다면 => 사번을 입력하세요! alert창 띄우기
+		//비밀번호가 비어있다면 => 비밀번호를 입력하세요! alert창 띄우기
+		if(id==null) {
+			
+		}
+		if(id==EmployeeDAO.getId) {
+			cookie.add
+		}
 	}
 
 }
