b60dc164398663b1e7c2502b0e1a6fc7b52fef30
parkhanju <parkhanju95@gmail.com>
2022-06-23 17:46:57 +0900

    first commit

---
+++ src/employee/model/EmployeeBean.java
@@ -0,0 +1,13 @@
+package employee.model;
+
+import java.sql.Date;
+
+public class EmployeeBean {
+	private int employeeId;				// 사원번호
+	private String employeeName; 		// 사원이름
+	private String employeePasswd;  	// 비밀번호
+	private String employeeAddr;		// 사원주소
+	private String employeeEmail;		// 사원메일
+	private String employeePhone;		// 사원연락처
+	private Date employeeHiredate;		// 사원입사일
+}
